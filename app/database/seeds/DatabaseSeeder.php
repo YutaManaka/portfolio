b979d1109805897d1ceff4147995c65cc93313c1
YutaManaka <54618797+YutaManaka@users.noreply.github.com>
2020-12-31 20:12:03 +0900

    ec#3

--- app/database/seeds/DatabaseSeeder.php
+++ app/database/seeds/DatabaseSeeder.php
@@ -11,6 +11,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+    	$this->call(StockTableSeeder::class); //追記
         // $this->call(UsersTableSeeder::class);
     }
 }
