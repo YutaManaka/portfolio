2749cdda1bd21d44050da87a7b9476543ec620d6
YutaManaka <54618797+YutaManaka@users.noreply.github.com>
2021-01-01 13:54:38 +0900

    ec#44

--- app/database/seeds/StockTableSeeder.php
+++ app/database/seeds/StockTableSeeder.php
@@ -14,8 +14,8 @@ class StockTableSeeder extends Seeder
     {
     	DB::table('stocks')->truncate(); //2回目実行の際にシーダー情報をクリア
     	DB::table('stocks')->insert([
-        	'name' => 'ランバン エクラドゥアルページュ',
-            'detail' => '定番の男ウケ･モテ香水として人気の香水',
+        	'name' => 'エクラドゥアルページュ',
+            'detail' => '定番の男ウケ･モテ香水',
             'fee' => 3780,
             'imgpath' => 'arpage.jpg',
         ]);
@@ -29,14 +29,14 @@ class StockTableSeeder extends Seeder
 
         DB::table('stocks')->insert([
             'name' => 'D&Gライトブルー',
-            'detail' => 'ドルガバの超人気香水!',
+            'detail' => 'ドルガバの超人気香水',
             'fee' => 2990,
             'imgpath' => 'lightblue.jpg',
         ]);
 
         DB::table('stocks')->insert([
-            'name' => 'D&Gライトブルー イタリアンゼスト',
-            'detail' => 'ライトブルーをより爽やかなにアレンジ',
+            'name' => 'D&Gイタリアンゼスト',
+            'detail' => 'ライトブルーを爽やかにアレンジ',
             'fee' => 3480,
             'imgpath' => 'lightbluezest.jpg',
         ]);
@@ -44,84 +44,84 @@ class StockTableSeeder extends Seeder
 
         DB::table('stocks')->insert([
             'name' => 'カルバンクライン エタニティ',
-            'detail' => 'カルバンクラインを代表する女性用香水。',
+            'detail' => 'カルバンクラインの代表的な香水',
             'fee' => 3500,
             'imgpath' => 'eternity.jpg',
         ]);
 
         DB::table('stocks')->insert([
             'name' => 'プラダ キャンディ',
-            'detail' => 'バニラ系の代表的な香水。',
+            'detail' => 'バニラ系の代表的な香水',
             'fee' => 4800,
             'imgpath' => 'candy.jpg',
         ]);
 
         DB::table('stocks')->insert([
             'name' => 'ギルティ バイ グッチ',
-            'detail' => 'グッチを代表する人気香水。',
+            'detail' => 'グッチを代表する人気香水',
             'fee' => 4800,
             'imgpath' => 'guilty.jpg',
         ]);
 
         DB::table('stocks')->insert([
             'name' => 'グッチ ブルーム',
-            'detail' => '香水の王道、フローラル系の香り。',
+            'detail' => '香水の王道、フローラル系の香り',
             'fee' => 5200,
             'imgpath' => 'bloom.jpg',
         ]);
 
         DB::table('stocks')->insert([
             'name' => 'グッチ ラッシュ2',
-            'detail' => '既存の香水の概念を覆した歴史的な香水。',
+            'detail' => '既存の概念を覆した歴史的香水',
             'fee' => 3200,
             'imgpath' => 'rush2.jpg',
         ]);
 
         DB::table('stocks')->insert([
             'name' => 'エルメス 屋根の上の庭',
-            'detail' => '爽やかな香りが特徴の香水。',
+            'detail' => '爽やかな香りが特徴の香水',
             'fee' => 5200,
             'imgpath' => 'hermes1.jpg',
         ]);
 
         DB::table('stocks')->insert([
             'name' => 'エルメス ラグーナの庭',
-            'detail' => '「庭シリーズ」の最新作の香水。',
+            'detail' => '「庭シリーズ」の最新作の香水',
             'fee' => 5200,
             'imgpath' => 'hermes2.jpg',
         ]);
 
         DB::table('stocks')->insert([
             'name' => 'ティファニー オードパルファム',
-            'detail' => '初心者におすすめの香水。',
+            'detail' => '初心者におすすめの香水',
             'fee' => 6600,
             'imgpath' => 'tiffany.jpg',
         ]);
 
         DB::table('stocks')->insert([
             'name' => 'サムライウーマン',
-            'detail' => 'サムライブランドの女性用香水。',
+            'detail' => 'サムライブランドの女性用香水',
             'fee' => 3600,
             'imgpath' => 'samourai.jpg',
         ]);
 
         DB::table('stocks')->insert([
             'name' => 'ライオンハート',
-            'detail' => '恋する女性におすすめのフルーティー系の香り。',
+            'detail' => '恋する女性におすすめの香り',
             'fee' => 2200,
             'imgpath' => 'lion.jpg',
         ]);
 
         DB::table('stocks')->insert([
             'name' => 'ローズドクロエ',
-            'detail' => '大人気のクロエの香りを大人っぽくアレンジ。',
+            'detail' => 'クロエの香りを大人っぽくアレンジ',
             'fee' => 4200,
             'imgpath' => 'rosecloe.jpg',
         ]);
 
         DB::table('stocks')->insert([
             'name' => 'フィアンセ',
-            'detail' => '万人ウケする香りが特徴の石鹸系香水。',
+            'detail' => '万人ウケする香りの石鹸系香水',
             'fee' => 980,
             'imgpath' => 'fiance.jpg',
         ]);
